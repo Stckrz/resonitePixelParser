e6202b92573ca0cf7c913f7fe8f279d63317a803
Stckrz <rin449@gmail.com>
2024-08-23 11:04:23 -0500

    some junk

---
+++ pixelSocketServer/client/client.js
@@ -0,0 +1,22 @@
+const WebSocket = require('ws');
+
+const ws = new WebSocket('ws://localhost:8080/');
+
+ws.on('open', () => {
+	console.log("Websocket connection open");
+	ws.send('hello server!')
+});
+
+ws.on('message', (message) => {
+	console.log(message.toString());
+});
+
+ws.on('close', (event) => {
+	console.log('Websocket connection closed', event);
+});
+
+ws.on('error', (error) => {
+	console.error('Websocket Error: ', error);
+});
+
+
--- pixelSocketServer/pixelSocketServer.js
+++ pixelSocketServer/pixelSocketServer.js
@@ -11,26 +11,13 @@ wss.on('connection', (client) => {
 
 		const textMessage = message.toString();
 
-		if (textMessage === 'br55667') {
-			wss.clients.forEach((ws) => {
-				console.log('br55667 read')
-				if (client.readyState === WebSocket.OPEN) {
-					client.send(textMessage);
-				}
-			})
-		} else {
-			wss.clients.forEach((client) => {
-				if (client.readyState === WebSocket.OPEN) {
-					client.send(textMessage);
-				}
-			});
-		}
-	});
+		wss.clients.forEach((client) => {
+			if (client.readyState === WebSocket.OPEN) {
+				client.send(textMessage);
+			}
+		})
 
-	// ws.on('message', (message) => {
-	// 	console.log('Got a message!', message.toString());
-	// 	ws.send(message.toString());
-	// });
+	});
 
 	client.on('close', () => {
 		console.log('client disconnected..');
@@ -47,3 +34,17 @@ console.log('websocket server is listening on port 8080');
 
 
 
+// if (textMessage === 'br55667') {
+// 	wss.clients.forEach((ws) => {
+// 		console.log('br55667 read')
+// 		if (client.readyState === WebSocket.OPEN) {
+// 			client.send(textMessage);
+// 		}
+// 	})
+// } else {
+// 	wss.clients.forEach((client) => {
+// 		if (client.readyState === WebSocket.OPEN) {
+// 			client.send(textMessage);
+// 		}
+// 	});
+// }
